9ee40bd72f2beb9552fd38d53cf461a039edfb74
WINDELL-7QOT3NS\mjdbr <michael_1984@interia.pl>
2026-05-14 17:40:07 +0200

    move state-mgmt to cloud-storage (created manually).

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~>3.100"
     }
   }
+
+  # this stores state-mgmt info in cloud, instead of locally (enterprise standard)
+  backend "azurerm" {
+		    resource_group_name  = "rg-terraform-state"
+		    storage_account_name = "tfstateragdemo123"
+		    container_name       = "tfstate"
+		    key                  = "rag-demo.tfstate"
+  }
 }
 
 provider "azurerm" {
