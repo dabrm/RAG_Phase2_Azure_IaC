7bbae5538cda5929bd82d631aeac660f4ec23299
WINDELL-7QOT3NS\mjdbr <michael_1984@interia.pl>
2026-05-14 17:08:20 +0200

    changed region for search-service only (lack of capacity on Azure side)

--- terraform/search.tf
+++ terraform/search.tf
@@ -1,7 +1,8 @@
 resource "azurerm_search_service" "search" {
   name                = "rag-search-demo-mdab01"
   resource_group_name = azurerm_resource_group.rag_rg.name
-  location            = azurerm_resource_group.rag_rg.location
+  #location            = azurerm_resource_group.rag_rg.location
+  location            = "eastus" #  everything sits on "eastus2", but Azure has no more search capacity available in that region.
 
   sku = "basic"
   
