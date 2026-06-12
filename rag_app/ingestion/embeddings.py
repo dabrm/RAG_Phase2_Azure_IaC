400248e15c2e7a32656cec0da2a52ce6a98fb4af
WINDELL-7QOT3NS\mjdbr <michael_1984@interia.pl>
2026-06-13 00:16:03 +0200

    .

--- rag_app/ingestion/embeddings.py
+++ rag_app/ingestion/embeddings.py
@@ -1,9 +1,9 @@
-from rag_app.core.clients import openai_client
+from rag_app.core.clients import get_openai_client()
 from rag_app.core.config import settings
 
 
 def generate_embedding(text: str):
-
+    openai_client = get_openai_client()
     response = openai_client.embeddings.create(
         model=settings.azure_openai_embedding_deployment,
         input=text
