5d003f731d74f1dd057fc56d58794e3955fab7f5
WINDELL-7QOT3NS\mjdbr <michael_1984@interia.pl>
2026-06-13 22:48:59 +0200

    search fixed

--- rag_app/retrieval/search.py
+++ rag_app/retrieval/search.py
@@ -6,6 +6,10 @@ from rag_app.core.config import settings
 
 
 def vector_search(query: str):
+    query = query.strip()
+
+    if not query:
+        raise ValueError("Query cannot be empty.")
 
     embedding = generate_embeddings([query])[0]
 
@@ -18,9 +22,9 @@ def vector_search(query: str):
     search_client = get_search_client()
 
     results = search_client.search(
-        search_text=None,
+        search_text=query,
         vector_queries=[vector_query],
-        select=["content", "source", "title"]
+        select=["content","source","title","chunk_index"]
     )
 
     return list(results)
